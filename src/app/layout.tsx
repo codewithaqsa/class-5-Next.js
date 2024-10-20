3c38032c1a526f575729a448ffc10f3a89464863
codewithaqsa <mmuneerkhan1963@gmail.com>
2024-10-20 19:07:56 +0500

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
+
+export default layout;
